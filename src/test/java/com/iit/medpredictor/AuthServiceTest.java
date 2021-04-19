7959c1bd0af995294bc09e595c0116ff267ed320
tharuash <tharindushane002@gmail.com>
2021-04-20 02:31:38 +0530

    Tests Added.

---
+++ src/test/java/com/iit/medpredictor/AuthServiceTest.java
@@ -0,0 +1,36 @@
+package com.iit.medpredictor;
+
+import com.iit.medpredictor.dto.AuthRequest;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class AuthServiceTest extends AbstractTest
+{
+	@Override
+	@Before
+	public void setUp() {
+		super.setUp();
+	}
+
+	@Test
+	public void userShouldAuthenticatedWhenUsernameAndPasswordReceived() throws Exception {
+		String uri = "/auth/login";
+		AuthRequest authRequest = new AuthRequest( "tharu" , "123" , "");
+		String inputJson = super.mapToJson(authRequest);
+		MvcResult mvcResult = mvc.perform( MockMvcRequestBuilders.post(uri)
+				.contentType( MediaType.APPLICATION_JSON_VALUE)
+				.content( inputJson ))
+				.andReturn();
+
+		int status = mvcResult.getResponse().getStatus();
+		assertEquals(200, status);
+		String response = mvcResult.getResponse().getContentAsString();
+		assertTrue( !response.contains( "incorrect" ) );
+	}
+}
---
+++ src/test/java/com/iit/medpredictor/OrderServiceTest.java
@@ -0,0 +1,46 @@
+package com.iit.medpredictor;
+
+import com.iit.medpredictor.entity.Order;
+import com.iit.medpredictor.entity.type.Medicine;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.junit.Assert.assertEquals;
+
+public class OrderServiceTest extends AbstractTest
+{
+	@Override
+	@Before
+	public void setUp() {
+		super.setUp();
+	}
+
+	@Test
+	public void latestOrdersListShouldReturnWhenOrdersHasRequested() throws Exception {
+		String uri = "/orders";
+		MvcResult mvcResult = mvc.perform( MockMvcRequestBuilders.get(uri)
+				.accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+
+		int status = mvcResult.getResponse().getStatus();
+		assertEquals(200, status);
+	}
+
+	@Test
+	public void orderShouldAddWhenOrderCreatedRequested() throws Exception {
+		String uri = "/orders?uid=1";
+		Order order = new Order( 0L, Medicine.ASPIRIN, 2001, (short)1, 500, true, "test note", null );
+		String inputJson = super.mapToJson(order);
+		MvcResult mvcResult = mvc.perform( MockMvcRequestBuilders.post(uri)
+				.contentType(MediaType.APPLICATION_JSON_VALUE)
+				.content( inputJson ))
+				.andReturn();
+
+		int status = mvcResult.getResponse().getStatus();
+		assertEquals(201, status);
+	}
+
+
+}
