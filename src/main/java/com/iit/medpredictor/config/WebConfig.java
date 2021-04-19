8ad357ea3fa7486999b109ab0db57ca92c0091e1
tharuash <tharindushane002@gmail.com>
2021-04-20 00:35:55 +0530

    Cors added.

--- src/main/java/com/iit/medpredictor/config/WebConfig.java
+++ src/main/java/com/iit/medpredictor/config/WebConfig.java
@@ -10,6 +10,6 @@ public class WebConfig implements WebMvcConfigurer
 	@Override
 	public void addCorsMappings( CorsRegistry registry )
 	{
-		registry.addMapping( "/**" ).allowedOrigins( "http://localhost:4200" ).allowedMethods( "GET", "POST","PUT","DELETE", "PATCH" );
+		registry.addMapping( "/**" ).allowedOrigins( "*" ).allowedMethods( "GET", "POST","PUT","DELETE", "PATCH" );
 	}
 }
